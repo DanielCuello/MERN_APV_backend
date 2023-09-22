7cd47814f0c08f2809534f8085c8c5a6f5b0d2fc
DanielCuello <113527181+DanielCuello@users.noreply.github.com>
2023-09-22 15:32:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import conectarDB from "./config/db.js";
-import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import veterinarioRoutes from "./routes/VeterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
 
 const app = express();
